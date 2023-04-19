04d872ac803bd16a70b53b8b3af349bbaa9c2ee6
Zenos03 <xavier.evans2550@gmail.com>
2023-04-19 10:50:21 -0400

    Added signals to buttons
    Still need to actually code the buttons

---
+++ Final Project/UI/Main Menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _ready():
+	pass
+
+func _on_Play_pressed():
+	pass
+
+
+func _on_Quit_pressed():
+	pass #
---
+++ Final Project/UI/Pause.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _ready():
+	pass
+
+func _on_Resume_pressed():
+	pass 
+
+func _on_Quit_pressed():
+	pass 
