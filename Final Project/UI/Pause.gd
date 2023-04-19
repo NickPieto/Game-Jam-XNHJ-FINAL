bf866cdc3dae63ef51eecae835b4e037efe569b5
Zenos03 <xavier.evans2550@gmail.com>
2023-04-19 11:28:37 -0400

    Buttons should work now

--- Final Project/UI/Pause.gd
+++ Final Project/UI/Pause.gd
@@ -1,11 +1,14 @@
 extends Control
 
+var Global
 
 func _ready():
 	pass
 
 func _on_Resume_pressed():
-	pass 
+	get_tree().paused = false
+	Global.reset()
+	var _scene = get_tree().change_scene("res://Game.tscn")
 
 func _on_Quit_pressed():
-	pass 
+	get_tree().quit()
