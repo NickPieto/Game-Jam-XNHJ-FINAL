0838b49ae24ddb4e29e87437a18bdf26843949a8
harv-wheel <hahawhee@iu.edu>
2023-04-20 00:11:47 -0400

    Adds State Machine
    Still need to mess with values for feel. controlled by variables in Player.gd

--- Final Project/Player/Player.gd
+++ Final Project/Player/Player.gd
@@ -1,25 +1,35 @@
-extends Node
+extends KinematicBody2D
 
-var velocity = 1
-var speed = 1
-var jumping_speed = 1
+onready var SM = $StateMachine
 
-func _ready():
-	pass
+var velocity = Vector2.ZERO
+var jump_power = Vector2.ZERO
+var direction = 1
+
+export var gravity = Vector2(0,30)
+
+export var move_speed = 5
+export var max_move = 200
+
+export var jump_speed = 1000 #Set jump speed and max jump for a single, instantaneous jump
+export var max_jump = 1000
+
+export var leap_speed = 5
+
 
 func _physics_process(_delta):
-	velocity = Vector2.ZERO
-	if Input.is_action_pressed("backward"):
-		velocity.x -= speed
-	if Input.is_action_pressed("forward"):
-		velocity.x += speed 
-	if Input.is_action_pressed("jump"):
-		velocity.y -= jumping_speed
+	velocity.x = clamp(velocity.x,-max_move,max_move)
+		
+	if direction < 0 and not $AnimatedSprite.flip_h: $AnimatedSprite.flip_h = true
+	if direction > 0 and $AnimatedSprite.flip_h: $AnimatedSprite.flip_h = false
+	
+	if $State.text != SM.state_name: #Changes the text of label "State".
+		$State.text = SM.state_name
 
-func is_moving():
-	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
-		return true
-	return false
+func set_direction(d):
+	direction = d
 
-func move_vector():
-	return Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"),1.0)
+func set_animation(anim):
+	if $AnimatedSprite.animation == anim: return
+	if $AnimatedSprite.frames.has_animation(anim): $AnimatedSprite.play(anim)
+	else: $AnimatedSprite.play()
