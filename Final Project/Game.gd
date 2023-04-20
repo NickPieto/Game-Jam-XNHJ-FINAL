45b8bb364bfc9e05dd6f95d02a71b3a8a2b53906
Zenos03 <xavier.evans2550@gmail.com>
2023-04-20 11:55:34 -0400

    Update Game.gd

--- Final Project/Game.gd
+++ Final Project/Game.gd
@@ -4,6 +4,6 @@ extends Node2D
 func _ready():
 	pass
 
-func physics_process(delta):
+func physics_process(_delta):
 	if Input.action_is_pressed("pause"):
 		var _scene = get_tree().change_scene("res://UI/Pause.tscn")
